e962100459828aeee9d6cf436794cf1ed5beee19
gando537 <gandohd@gmail.com>
2023-10-16 00:06:13 +0200

    finalist

---
+++ src/app/header/header-dummy-data.ts
@@ -0,0 +1,59 @@
+export const languages = [
+    {
+        language: 'English',
+        flag: 'us'
+    },
+    {
+        language: 'French',
+        flag: 'france'
+    },
+    {
+        language: 'German',
+        flag: 'germany'
+    },
+    {
+        language: 'Russian',
+        flag: 'russia'
+    },
+    {
+        language: 'Spanish',
+        flag: 'spain'
+    }
+];
+
+export const notifications = [
+    {
+        icon: 'far fa-cloud-download',
+        subject: 'Download Completed',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+        icon: 'far fa-cloud-upload',
+        subject: 'Upload Completed',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+        icon: 'far fa-cloud-download',
+        subject: '350 MB trash files',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    }
+];
+
+export const userItems = [
+    {
+        icon: 'far fa-user',
+        label: 'Profile'
+    },
+    {
+        icon: 'far fa-cog',
+        label: 'Settings'
+    },
+    {
+        icon: 'far fa-unlock-alt',
+        label: 'Lock screen'
+    },
+    {
+        icon: 'far fa-power-off',
+        label: 'Logout'
+    },
+];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { languages, notifications, userItems } from './header-dummy-data';
 
 @Component({
   selector: 'app-header',
@@ -11,6 +12,11 @@ export class HeaderComponent implements OnInit {
   @Input() screenWidth: number = 0;
 
   canShowSearchAsOverlay: boolean = false;
+  selectedLanguage: any;
+
+  languages = languages;
+  notifications = notifications;
+  userItems = userItems;
 
   constructor() { }
 
@@ -22,6 +28,7 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
     this.checkCanShowSearchAsOverlay(window.innerWidth);
+    this.selectedLanguage = this.languages[0];
   }
 
   getHeadClass(): string {
